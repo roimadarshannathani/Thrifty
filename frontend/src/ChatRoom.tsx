eae131e2eff4a7e07eeb78b30aa5f62fbdb2f25d
roimadarshannathani <darshannathani@roimaint.com>
2025-07-03 13:48:30 +0530

    fix: connection route link fix

--- frontend/src/ChatRoom.tsx
+++ frontend/src/ChatRoom.tsx
@@ -15,7 +15,7 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ name }) => {
     console.log(window)
     // const { protocol, hostname } = window.location;
     // const wsPort = 8080; // Change if your backend uses a different port in production
-    const wsUrl = `http://ip172-18-0-55-d1j24b291nsg009j0kqg-8080.direct.labs.play-with-docker.com//ws`;
+    const wsUrl = `http://ip172-18-0-55-d1j24b291nsg009j0kqg-8080.direct.labs.play-with-docker.com/ws`;
     const socket = new SockJS(wsUrl);
     const stompClient = Stomp.over(socket);
     stompClient.connect({}, () => {
